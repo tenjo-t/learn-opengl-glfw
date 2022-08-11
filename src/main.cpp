3ef5e7f2d68893ed7f968f4d8e11a48d6c0b3b04
tenjo-t <tenjo0314takashi@gmail.com>
2022-08-11 15:56:15 +0900

    section 4.2.6

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,42 @@
 #include <cstdlib>
 #include <iostream>
 
+// プログラムオブジェクトを作成する
+//   vsrc: バーテックスシェーダのソースプログラムの文字列
+//   fsrc: フラグメントシェーダのソースプログラムの文字列
+GLuint createProgram(const char *vsrc, const char *fsrc) {
+    // 空のプログラムオブジェクトを作成する
+    const GLuint program(glCreateProgram());
+
+    if (vsrc != NULL) {
+        // バーテックスシェーダのシェーダオブジェクトを作成する
+        const GLuint vobj(glCreateShader(GL_VERTEX_SHADER));
+        glShaderSource(vobj, 1, &vsrc, NULL);
+        glCompileShader(vobj);
+
+        // バーテックスシェーダのシェーダオブジェクトをプログラムに組み込む
+        glAttachShader(program, vobj);
+        glDeleteShader(vobj);
+    }
+
+    if (fsrc != NULL) {
+        // フラグメントシェーダのシェーダオブジェクトを作成する
+        const GLuint fobj(glCreateShader(GL_FRAGMENT_SHADER));
+        glShaderSource(fobj, 1, &fsrc, NULL);
+
+        // フラグメントシェーダのシェーダオブジェクトをプログラムに組み込む
+        glAttachShader(program, fobj);
+        glDeleteShader(fobj);
+    }
+
+    // プログラムオブジェクトをリンクする
+    glBindAttribLocation(program, 0, "position");
+    glBindFragDataLocation(program, 0, "fragment");
+
+    // 作成したプログラムオブジェクトを返す
+    return program;
+}
+
 int main() {
     // GLFW を初期化する
     if (glfwInit() == GL_FALSE) {
